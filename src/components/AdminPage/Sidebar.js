d63fa1857025c45d4fab42e28a61c06e62a648e3
MuttakinHasib <hasibmolla28@gmail.com>
2021-06-12 16:21:04 +0600

    :hammer:

--- src/components/AdminPage/Sidebar.js
+++ src/components/AdminPage/Sidebar.js
@@ -14,31 +14,31 @@ const Sidebar = () => {
         Refcart
       </div>
       <div className='py-5'>
-        <Link href='/admin'>
+        <Link href='/admin/admin'>
           <a className='flex gap-3 items-center my-1 px-4 py-3 text-white border-l-4 border-blue-600 bg-gray-900'>
             <HomeIcon className='w-5' />
             <span>Dashboard</span>
           </a>
         </Link>
-        <Link href='/category'>
+        <Link href='/admin/category'>
           <a className='flex gap-3 items-center my-1 px-4 py-3 text-white border-l-4 border-transparent hover:border-blue-600 hover:bg-gray-900 transition duration-300'>
             <CollectionIcon className='w-5' />
             <span>Category</span>
           </a>
         </Link>
-        <Link href='/products'>
+        <Link href='/admin/products'>
           <a className='flex gap-3 items-center my-1 px-4 py-3 text-white border-l-4 border-transparent hover:border-blue-600 hover:bg-gray-900 transition duration-300'>
             <ShoppingBagIcon className='w-5' />
             <span>Products</span>
           </a>
         </Link>
-        <Link href='/orders'>
+        <Link href='/admin/orders'>
           <a className='flex gap-3 items-center my-1 px-4 py-3 text-white border-l-4 border-transparent hover:border-blue-600 hover:bg-gray-900 transition duration-300'>
             <ClipboardListIcon className='w-5' />
             <span>Orders</span>
           </a>
         </Link>
-        <Link href='/users'>
+        <Link href='/admin/users'>
           <a className='flex gap-3 items-center my-1 px-4 py-3 text-white border-l-4 border-transparent hover:border-blue-600 hover:bg-gray-900 transition duration-300'>
             <UsersIcon className='w-5' />
             <span>Users</span>
