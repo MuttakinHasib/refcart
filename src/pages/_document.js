34513ec535d53a6a50de13f2d24dcb52d4c9a8b5
MuttakinHasib <hasibmolla28@gmail.com>
2021-07-19 18:16:54 +0600

    added some SEO staff :zap:

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,32 +5,32 @@ class MyDocument extends Document {
     return (
       <Html lang='en'>
         <Head>
-          {/* <meta name='image' content='/banner.png' /> */}
-          <meta property='og:title' content='Hasib Molla' />
+          <meta name='image' content='/seo_image.png' />
+          <meta property='og:title' content='Refcart | Online Shopping Mall' />
           <meta
             name='google-site-verification'
             content='LUBT9DCkD_bj06Z0b5imXXQiZXiwadd3gYjzqN9yDy8'
           />
           <meta
             name='description'
-            content='Hasib is a passionate self-taught Full Stack web developer from Bangladesh who develop web applications, mobile applications and desktop applications.'
+            content='Refcart is an e-commerce site which will be capable of providing every kind of goods and products from every sector to every consumer located in Bangladesh.'
           />
           <meta
             property='og:description'
-            content='Hasib is a passionate self-taught Full Stack web developer from Bangladesh who develop web applications, mobile applications and desktop applications.'
+            content='Refcart is an e-commerce site which will be capable of providing every kind of goods and products from every sector to every consumer located in Bangladesh.'
           />
-          {/* <meta property='og:image' content='/banner.png' /> */}
-          <meta property='og:url' content='https://muttakinhasib.github.io/' />
+          <meta property='og:image' content='/seo_image.png' />
+          <meta property='og:url' content='https://refcart.vercel.app/' />
           <meta property='og:type' content='website' />
 
           <meta name='twitter:card' content='summary_large_image' />
           <meta name='twitter:creator' content='@MuttakinHasib' />
-          <meta name='twitter:title' content='Hasib Molla' />
+          <meta name='twitter:title' content='Refcart | Online Shopping Mall' />
           <meta
             name='twitter:description'
-            content='Hasib is a passionate self-taught Full Stack web developer from Bangladesh who develop web applications, mobile applications and desktop applications '
+            content='Refcart is an e-commerce site which will be capable of providing every kind of goods and products from every sector to every consumer located in Bangladesh.'
           />
-          {/* <meta name='twitter:image' content='/banner.png' /> */}
+          <meta name='twitter:image' content='/seo_image.png' />
 
           <link rel='preconnect' href='https://fonts.gstatic.com' />
           <link
